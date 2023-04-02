8ed66e8994a924aa6669125891f1de4d79bd3ed2
Plizama <pamela.lizama@usach.cl>
2023-04-01 23:48:43 -0400

    capas selectoras y funciones 1 a la 3

--- funciones_17325089_LizamaNunez.rkt
+++ funciones_17325089_LizamaNunez.rkt
@@ -38,3 +38,31 @@
   (list name-user))
 
 ;;SELECTOR
+;; Nombre systema
+(define get-nameSystem car)
+;; lista de drives
+(define get-drives-ingresados cadr)
+;; lista de usuarios ingresados
+(define get-users caddr)
+;;usuario logeado
+(define get-user-log cadddr)
+;;drive en uso actualmente
+(define (get-drive-actual system)
+  (car (reverse system)))
+
+;; modificadora
+
+
+;; type filesystem = list name X drives-ingresados X users X log X drive_actual
+
+;; Funcion 1: Creacion de nuevos sistemas
+(define (system nombre)
+  (list nombre '() '() "" ""  ))
+
+;; Funcion 2 : TDA system -run
+(define(run system command)
+  (command system))
+
+;; Funcion 3: TDA system - add-drive
+(define (addDrive system letter nameDrive capacity)
+  (make-system (get-nameSystem system)(make-drives-ingresados(make-letters letter nameDrive capacity "" '()))(get-users system)(get-user-log system)(get-drive-actual system)))
