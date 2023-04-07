0af770960a26e15fe524653407ae712bc550bccc
Plizama <pamela.lizama@usach.cl>
2023-04-06 20:47:23 -0400

    Arreglo funciones

--- funciones_17325089_LizamaNunez.rkt
+++ funciones_17325089_LizamaNunez.rkt
@@ -4,9 +4,9 @@
 ;;CONSTRUCTOR
 
 ;; Constructor system 
-(define (make-system name drives-ingresados users log drive-actual trash fechaCreacion)
-  (list name drives-ingresados users log drive-actual trash fechaCreacion))
-;;P--(make-system "hola" "C" "pamela" "luko" "D")
+(define (make-system name drives-ingresados users log drive-actual trash fechaCreacion directory)
+  (list name drives-ingresados users log drive-actual trash fechaCreacion directory))
+
 
 ;; Constructor drives-ingresados
 (define (make-drives-ingresados letters)
@@ -23,8 +23,8 @@
 (define (make-name-directory name-directory)
   (list name-directory))
 
-(define (make-directory name ruta user-creator fecha-creacion fecha-modificacion seguridad file)
-  (list name ruta user-creator fecha-creacion fecha-modificacion seguridad file))
+(define (make-directory name ruta user-creator fecha-creacion fecha-modificacion seguridad)
+  (list name ruta user-creator fecha-creacion fecha-modificacion seguridad))
 
 ;; Constructor file 
 (define (make-name-file name-file)
@@ -50,21 +50,25 @@
 (define get-user-log cadddr)
 ;; drive en uso actualmente
 (define (get-drive-actual system)
-  (car( cdr (cdr(reverse system)))))
+  (car( cdr (cdr(cdr(reverse system))))))
 ;; trash
 (define (get-trash system)
-  (car ( cdr(reverse system))))
+  (car( cdr (cdr(reverse system)))))
 ;; fechaCreacion
-(define (get-fechaCreacion)
+(define (get-fechaCreacion system)
+  (car ( cdr(reverse system))))
+;; directory
+(define (get-directory system)
   (car(reverse system)))
+
 
 ;; MODIFICADORES
 
-;; type filesystem = list name X drives-ingresados X users X log X drive_actual X trash X fechaCreacion 
+;; type filesystem = list name X drives-ingresados X users X log X drive_actual X trash X fechaCreacion X directory
 
 ;; Funcion 1: Creacion de nuevos sistemas
 (define (system nombre)
-  (list nombre '() '() "" "" '() make-fechaCreacion ))
+  (list nombre '() '() "" "" '() make-fechaCreacion '() ))
 
 ;; Funcion 2 : TDA system -run
 (define(run system command)
@@ -74,29 +78,33 @@
 ;; Funcion 3: TDA system - add-drive
 (define (add-drive system)
     (lambda (letter nameDrive capacity)
-      (make-system (get-nameSystem system)(cons (make-letters letter nameDrive capacity "" '())(get-drives-ingresados system))(get-users system)(get-user-log system)(get-drive-actual system)(get-trash system) make-fechaCreacion)))
+      (make-system (get-nameSystem system)(cons (make-letters letter nameDrive capacity "" '())(get-drives-ingresados system))(get-users system)(get-user-log system)(get-drive-actual system)(get-trash system) make-fechaCreacion (get-directory system))))
 
 ;; Funcion 4: TDA system -register
 (define (add-user system)
   (lambda (userName)
-    (make-system (get-nameSystem system)(get-drives-ingresados system)(cons userName (get-users system))(get-user-log system)(get-drive-actual system)(get-trash system) make-fechaCreacion)))
+    (make-system (get-nameSystem system)(get-drives-ingresados system)(cons userName (get-users system))(get-user-log system)(get-drive-actual system)(get-trash system) make-fechaCreacion (get-directory system))))
 
 ;; Funcion 5: TDA system -login
 (define (login system)
   (lambda (userName-log)
     (cond
       [(eq? "" (get-user-log system))
-      (make-system (get-nameSystem system)(get-drives-ingresados system)(get-users system) userName-log (get-drive-actual system)(get-trash system) make-fechaCreacion)]
+      (make-system (get-nameSystem system)(get-drives-ingresados system)(get-users system) userName-log (get-drive-actual system)(get-trash system) make-fechaCreacion (get-directory system))]
       [else system])))
 
 ;; Funcion 6: TDA system logout
 (define (logout system)
-  ( make-system (get-nameSystem system)(get-drives-ingresados system)(get-users system) "" (get-drive-actual system)(get-trash system) make-fechaCreacion))
+  ( make-system (get-nameSystem system)(get-drives-ingresados system)(get-users system) "" (get-drive-actual system)(get-trash system) make-fechaCreacion (get-directory system)))
 
 ;; Funcion 7: TDA system - switch-drive
 (define(switch-drive system)
   (lambda (letter)
-    (make-system (get-nameSystem system)(get-drives-ingresados system)(get-users system) (get-user-log system) letter (get-trash system) make-fechaCreacion)))
+    (make-system (get-nameSystem system)(get-drives-ingresados system)(get-users system) (get-user-log system) letter (get-trash system) make-fechaCreacion (get-directory system))))
 
-;; type name-directory = list name X ruta X user-creador X fecha-creacion X fecha-modificacion X seguridad X file
+;; type name-directory = list name X ruta X user-creador X fecha-creacion X fecha-modificacion X seguridad
+;; crear ruta
 ;; Funcion 8: TDA system-md (make directory)
+(define (md system)
+  (lambda (nameDirectory)
+    (make-system (get-nameSystem system)(get-drives-ingresados system)(get-users system) (get-user-log system) (get-drive-actual system)(get-trash system) make-fechaCreacion (cons (make-directory nameDirectory (get-drive-actual system) (get-user-log system) make-fechaCreacion make-fechaCreacion "") (get-directory system)))))
